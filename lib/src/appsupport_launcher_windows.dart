2a9f01bc87c38fd1279278d360783acb3f76ccb8
Jappe02 <jasper.mannonen@gmail.com>
2026-07-19 17:39:12 +0300

    Improve launch logic and documentation

--- lib/src/appsupport_launcher_windows.dart
+++ lib/src/appsupport_launcher_windows.dart
@@ -1,20 +1,20 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
-
-import 'package:appsupport_launcher_windows/src/gui_dialog.dart';
 
 import 'application_identity.dart';
 import 'constants.dart';
+import 'gui_dialog.dart';
 import 'lock.dart';
+import 'prefix/prefix_identity.dart';
 import 'prefix/prefix_manager.dart';
+import 'process_runner.dart';
 import 'runtime/runtime.dart';
 import 'runtime/runtime_manager.dart';
 
 /// A program that allows running Windows OS executables in a Linux-based OS.
 sealed class AppSupportLauncherWindows {
   static bool _isRunning = false;
-  static Process? _currentProcess;
+  static ProcessRunner? _currentProcess;
 
   /// Main method that runs the entire application launcher.
   static Future<int> main(List<String> arguments) async {
@@ -36,7 +36,7 @@ sealed class AppSupportLauncherWindows {
         ProcessSignal.sigterm.watch().listen(_handleQuitSignal),
       ];
 
-      final code = await _launchApp(arguments[0]);
+      final code = await _launchApp(readLaunchCommand(arguments).appPath);
       print("Application or child exiting with code: $code");
       return code;
     } catch (e, stackTrace) {
@@ -60,58 +60,107 @@ sealed class AppSupportLauncherWindows {
   static Future<int> _launchApp(String appPath) async {
     final waitForInitDialog = GuiDialog.progress("Launching Windows Application...");
     Lock? prefixLock;
+
+    Future<void> cleanup() async {
+      await prefixLock?.tryClose();
+      await waitForInitDialog.close();
+    }
+
+    Future<ProcessRunner> launch(
+      Runtime runtime,
+      String appPath,
+      PrefixIdentity prefixIdentity,
+    ) async {
+      _currentProcess = await ProcessRunner.launch(runtime, appPath, prefixIdentity);
+      return _currentProcess!;
+    }
+
+    Future<void> lock(ApplicationIdentity app) async {
+      final prefixDir = Directory(getPrefixDirectoryForApp(app));
+      await prefixDir.create(recursive: true);
+      await prefixLock?.tryClose();
+      prefixLock = await Lock.acquire(prefixDir);
+    }
+
     try {
+      // Return early if the executable to be launched does not exist.
       if (!await File(appPath).exists()) {
         throw Exception('Application executable not found at path: $appPath');
       }
 
+      // Return early if no runtimes are present on the system.
       final runtimes = await RuntimeManager.listRuntimes();
       if (runtimes.isEmpty) {
         throw Exception('No runtimes available to launch the application.');
       }
 
+      // Read application identity data from the executable to be launched.
+      // This is associated with a prefix later.
       final identity = await ApplicationIdentity.readFromExe(appPath);
 
-      {
-        final prefixDir = Directory(getPrefixDirectoryForApp(identity));
-        if (await prefixDir.exists()) {
-          prefixLock = await Lock.acquire(prefixDir);
-        }
-      }
+      // Lock the prefix for the executable to be launched.
+      await lock(identity);
 
-      final prefixIdentity = await PrefixManager.getPrefix(identity);
+      // Get existing prefix for app, or null if none exists.
+      final prefixIdentity = await PrefixManager.getPrefixForExecutable(
+        identity,
+        appPath,
+      );
+
       final runtime = await RuntimeManager.getRuntime(prefixIdentity?.runtimeIdentity);
-      bool needsCreatePrefix = prefixIdentity == null;
+      final hasNoOriginalPrefix = prefixIdentity == null;
 
+      // If a prefix exists already, the app has been launched before, which
+      // means that we will try to launch it with the same runtime as before.
+      // If a prefix does not exist or the previously used runtime is not present,
+      // we will try the available runtimes in the loop below.
       if (prefixIdentity != null && runtime != null) {
-        final command = await runtime.getLaunchCommand(appPath, prefixIdentity);
-        final launch = await _launch(command);
-        if (launch.$1) {
-          await prefixLock?.close();
-          await waitForInitDialog.close();
-          return await launch.$2!.exitCode;
+        // Launch the app normally, and return the exit code future.
+        // If launch does not succeed, enter the loop below to try alternatives.
+        final launched = await launch(runtime, appPath, prefixIdentity);
+        if (launched.launchSuccess) {
+          await cleanup();
+          return (await launched.future)!;
         }
         print('Failed to launch app with runtime ${runtime.identity.name} version ${runtime.identity.version}. Trying other compatible runtimes...');
       }
 
-      // TODO: Write new working runtime to prefix data, so it's picked first the
-      //       next time.
+      // Try runtimes until the app launches with one of them.
       for (final runtime in runtimes) {
         print('Trying runtime ${runtime.identity.name} version ${runtime.identity.version}...');
         try {
-          if (prefixIdentity != null &&
+          // If an original prefix exists, only launch with runtimes that can
+          // share the same prefix. We do NOT want to overwrite the user's prefix.
+          if (!hasNoOriginalPrefix &&
               !runtime.isCompatibleWith(prefixIdentity.runtimeIdentity)) {
             continue;
           }
-          final prefix = needsCreatePrefix
-              ? await PrefixManager.createPrefix(runtime, identity, true)
-              : prefixIdentity;
-          final command = await runtime.getLaunchCommand(appPath, prefix);
-          final launch = await _launch(command);
-          if (launch.$1) {
-            await prefixLock?.close();
-            await waitForInitDialog.close();
-            return await launch.$2!.exitCode;
+
+          // If an original prefix did not exist, we will create a new one each
+          // time, so we go through all installed runtimes to check which one
+          // works for this app.
+          // If an original prefix exists, but could not be launched with the
+          // runtime it was last launched with, we will only overwrite metadata,
+          // so that the new working runtime will be the new default, and all
+          // prefix files are preserved.
+          final prefix = hasNoOriginalPrefix
+              ? await PrefixManager.createPrefix(
+                  runtime,
+                  identity,
+                  CreatePrefixMode.overwriteAll,
+                )
+              : await PrefixManager.createPrefix(
+                  runtime,
+                  identity,
+                  CreatePrefixMode.overwriteMetadata,
+                );
+
+          // Launch the app normally, and return the exit code future.
+          // If launch does not succeed, try with another runtime at the next iteration.
+          final launched = await launch(runtime, appPath, prefix);
+          if (launched.launchSuccess) {
+            await cleanup();
+            return (await launched.future)!;
           }
           print('Failed to launch app with runtime ${runtime.identity.name} version ${runtime.identity.version}: Launch command failed.');
         } catch (e) {
@@ -119,68 +168,17 @@ sealed class AppSupportLauncherWindows {
         }
       }
     } finally {
-      await prefixLock?.tryClose();
-      await waitForInitDialog.close();
+      await cleanup();
     }
 
+    // If no runtime was able to start the app:
     final errorString = 'Failed to launch app with any available runtime. Please ensure that a compatible runtime is installed and try again.';
     print(errorString);
     await GuiDialog.error(errorString).result();
     return 1;
   }
 
-  /// Performs the launch operation of an app, based on a [RuntimeLaunchInfo]
-  /// object. Returns true if launch succeeded, otherwise false. Should
-  /// never throw.
-  static Future<(bool, Process?)> _launch(RuntimeLaunchInfo launchInfo) async {
-    print('Launching app with command ${launchInfo.command}.');
-    Process? process;
-    try {
-      int? exitCode;
-      try {
-        process = await Process.start(
-          launchInfo.command.first,
-          launchInfo.command.skip(1).toList(),
-          environment: launchInfo.environment,
-          workingDirectory: launchInfo.workingDirectory,
-          mode: ProcessStartMode.normal,
-        );
-
-        process.stdout
-            .transform(utf8.decoder)
-            .forEach(stdout.write);
-
-        process.stderr
-            .transform(utf8.decoder)
-            .forEach(stderr.write);
-
-        exitCode = await process.exitCode.timeout(Duration(seconds: 10));
-      } on TimeoutException {
-        _currentProcess = process!;
-        return (true, process);
-      }
-
-      final stdoutRes = await process.stdout.transform(utf8.decoder).join();
-      final stderrRes = await process.stderr.transform(utf8.decoder).join();
-      if (exitCode != 0 && _isCompatToolFailure(exitCode, stdoutRes, stderrRes)) {
-        throw Exception('App exited with code $exitCode.');
-      }
-    } on ProcessException catch (e) {
-      print('Failed to launch app with command ${launchInfo.command}: Invalid runtime executable: $e');
-      return (false, null);
-    } catch (e) {
-      print('Failed to launch app with command ${launchInfo.command}: $e');
-      return (false, null);
-    }
-    _currentProcess = process;
-    return (true, process);
-  }
-
-  // TODO
-  static bool _isCompatToolFailure(int exitCode, String stdout, String stderr) {
-    return false;
-  }
-
+  /// Forward signals to compat-launched executable.
   static void _handleQuitSignal(ProcessSignal signal) {
     _currentProcess?.kill(signal);
   }
