98ac05b371825a65b630c08545a1655fdc6a154d
Jappe02 <jasper.mannonen@gmail.com>
2026-07-19 17:39:35 +0300

    Add directory getters and launch command functions

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -10,6 +10,19 @@ String getAppDataDirectory() {
   return p.join(homeDir, '.local', 'share', 'appsupport', 'win');
 }
 
+/// Gets the base application data directory.
+String getDesktopEntryDirectory() {
+  assert(Platform.isLinux);
+  final homeDir = Platform.environment['HOME']!;
+  return p.join(homeDir, '.local', 'share', 'applications');
+}
+
+/// Gets the path to this executable.
+Future<String> getAppExecutableDirectory() async {
+  assert(Platform.isLinux);
+  return File('/proc/self/exe').resolveSymbolicLinks();
+}
+
 /// The regex used for normalizing the app publisher and name. Used in
 /// [getPrefixDirectoryForApp], for example.
 const String appPublisherAndNameRegex = r'[^a-z0-9-_]';
@@ -37,4 +50,44 @@ String getPrefixDirectoryForApp(ApplicationIdentity appIdentity) {
 String getPrefixIdentityFilePath(ApplicationIdentity appIdentity) {
   final prefixDir = getPrefixDirectoryForApp(appIdentity);
   return p.join(prefixDir, 'prefix.json');
+}
+
+/// Reads an incoming launch command to a [LaunchCommand] instance.
+LaunchCommand readLaunchCommand(List<String> data) {
+  return LaunchCommand(data.firstOrNull ?? "");
+}
+
+/// Writes a launch command from [LaunchCommand] to a [List] of [String].
+Future<List<String>> writeLaunchCommand(LaunchCommand launchData) async {
+  return [await getAppExecutableDirectory(), launchData.appPath];
+}
+
+/// Writes a launch command returned by [writeLaunchCommand] to a string
+/// usable as a command directly.
+Future<String> writeLaunchCommandAsString(LaunchCommand launchData) async {
+  final cmd = await writeLaunchCommand(launchData);
+  String str = "";
+  for (var part in cmd) {
+    if (part.contains(" ")) {
+      part = '"$part"';
+    }
+    str += "$part ";
+  }
+
+  return str.trim();
+}
+
+/// Represents a launch command for this app.
+class LaunchCommand {
+  final String appPath;
+
+  const LaunchCommand(this.appPath);
+}
+
+/// Checks if the [child] path is somewhere inside the [parent] one.
+Future<bool> isReallyInside(Directory parent, FileSystemEntity child) async {
+  final parentResolved = await parent.resolveSymbolicLinks();
+  final childResolved = await child.resolveSymbolicLinks();
+
+  return p.isWithin(parentResolved, childResolved);
 }
